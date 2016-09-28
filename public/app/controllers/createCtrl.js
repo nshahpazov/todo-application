274362d67e2a65ea0656ec010ad516bc95507954
MShahpazova <mshahpazova@gmail.com>
2016-09-28 13:16:44 +0300

    test

--- public/app/controllers/createCtrl.js
+++ public/app/controllers/createCtrl.js
@@ -3,4 +3,5 @@ angular.module('todo')
   .controller('CreateCtrl', function ($scope) {
     $scope.firstName = 'Nikola';
     console.log('hello from CreateCtrl');
+    console.log('Hello from the other side');
   });
